0a2b26abd48fa2c72eab725f66bb36a3d2a56d89
Gabriel Nascimento <gsnaxsi@gmail.com>
2022-04-07 11:45:47 -0300

    Project Initialized

---
+++ assets/js/animation.js
@@ -0,0 +1,30 @@
+// gsap.from(".navbar", {
+//     duration: 1.5,
+//     opacity: 0,
+//     y: 80,
+//     ease: "power2.inOut"
+// });
+
+// gsap.from(".content > h1", {
+//     duration: 2,
+//     opacity: 0,
+//     y: 50,
+//     ease: "power2.inOut",
+//     delay: 0.3
+// });
+
+// gsap.from(".content > span", {
+//     duration: 2.2,
+//     opacity: 0,
+//     y: 80,
+//     ease: "power2.inOut",
+//     delay: 0.8
+// });
+
+// gsap.from(".content > button", {
+//     duration: 2.5,
+//     opacity: 0,
+//     y: 80,
+//     ease: "power2.inOut",
+//     delay: 0.8
+// });
